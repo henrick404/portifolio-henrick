9d7264fae0fc513785f031bffc93588041771d2c
henrick404 <160244828+henrick404@users.noreply.github.com>
2024-02-23 08:27:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const l1 = document.getElementById("jorge")
+
+function ligar() {
+    l1.src = "download.jpeg"
+}
+function desligar() {
+    l1.src = "ligado ao contrario.jpg"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
